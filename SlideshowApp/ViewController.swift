6969eacb1d75f9a55a1b578ebbb9b4db6aa800d3
Musashi Raijin <Raijin@raijin.local>
2016-07-29 17:20:57 +0900

    ビルド成功　起動後、１枚目の画像表示成功

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -23,14 +23,40 @@ class ViewController: UIViewController {
     }
     
     // UIImageViweのOutlet
-    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var imageView: UIImageView!
     
-    // 配列 picture　画像３枚、ライオン、トラ、チーター
-    let picture = ["lion.jpeg", "tiger.jpeg", "cheetah.jpeg"]
+    
+    // 配列 pictures　画像３枚、ライオン、トラ、チーター
+    let pictures = ["lion.jpeg", "tiger.jpeg", "cheetah.jpeg"]
+    
+    var myImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        // UIImageViewを作成する.
+        myImageView = UIImageView(frame: CGRectMake(0,0,400,200))
+        
+        // 表示する画像を設定する.
+        let myImage = UIImage(named: pictures[0])
+        
+        // 画像をUIImageViewに設定する.
+        myImageView.image = myImage
+        
+        // 画像の表示する座標を指定する.
+        myImageView.layer.position = CGPoint(x: self.view.bounds.width/2, y: 200.0)
+        
+        // UIImageViewをViewに追加する.
+        self.view.addSubview(myImageView)
+        
+        
+        
+        
+        
+    //  let image:UIImage! = UIImage(named: pictures[0])
+        
     }
 
     override func didReceiveMemoryWarning() {
